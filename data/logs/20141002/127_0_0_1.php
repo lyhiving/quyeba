9f7be4e24d4d791123c5e7c5469cda45a13b06f7
yfg2014 <2324704583@qq.com>
2014-10-02 22:18:36 +0800

    del someth

---
+++ data/logs/20141002/127_0_0_1.php
@@ -0,0 +1,5 @@
+<?php exit()?>
+2014-10-02 22:17:42	widget cache miss => app:,theme:tnf,type:nav_goodscat.
+2014-10-02 22:17:42	widget cache miss => app:,theme:tnf,type:goodscat_show.
+2014-10-02 22:17:43	widget cache miss => app:,theme:tnf,type:nav_goodscat.
+2014-10-02 22:17:43	widget cache miss => app:,theme:tnf,type:goodscat_show.
